bec5612b5eb93fe65ba33a6dc219946e7a2779a9
Kristian Virmo <kristian.virmo@digia.com>
2016-07-06 14:27:48 +0300

    Migrated RC2 schema to RTM. This was breaking when updating database from scratch.

--- HumidorClient/src/HumidorClient/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ HumidorClient/src/HumidorClient/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -18,7 +18,7 @@ namespace HumidorClient.Data.Migrations
                     Id = table.Column<string>(nullable: false),
                     ConcurrencyStamp = table.Column<string>(nullable: true),
                     Name = table.Column<string>(nullable: true),
-                    NormalizedName = table.Column<string>(nullable: true)
+                    NormalizedName = table.Column<string>(maxLength: 256, nullable: true)
                 },
                 constraints: table =>
                 {
@@ -50,8 +50,8 @@ namespace HumidorClient.Data.Migrations
                     EmailConfirmed = table.Column<bool>(nullable: false),
                     LockoutEnabled = table.Column<bool>(nullable: false),
                     LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
-                    NormalizedEmail = table.Column<string>(nullable: true),
-                    NormalizedUserName = table.Column<string>(nullable: true),
+                    NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
                     PasswordHash = table.Column<string>(nullable: true),
                     PhoneNumber = table.Column<string>(nullable: true),
                     PhoneNumberConfirmed = table.Column<bool>(nullable: false),
