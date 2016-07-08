15ea7f45e1b95fe76366db2e7bd4d21bbce57d96
Kristian Virmo <kristian.virmo@digia.com>
2016-07-08 15:46:47 +0300

    Added angular2 dependencies. Removed unwanted jquery dependencies.

--- HumidorClient/src/HumidorClient/wwwroot/_references.js
+++ HumidorClient/src/HumidorClient/wwwroot/_references.js
@@ -1,6 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
 /// <reference path="js/site.js" />
+/// <reference path="lib/angular2/angular.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/fotorama/fotorama.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
