884bf450ea004d14a8db7af9688d3ca77cd1cf32
Kristian Virmo <kristian.virmo@digia.com>
2016-07-11 14:29:33 +0300

    Added gulp tasks to copy node deps to frontend libs folder. Added script refs to layout file.

--- HumidorClient/src/HumidorClient/gulpfile.js
+++ HumidorClient/src/HumidorClient/gulpfile.js
@@ -8,41 +8,45 @@ var gulp = require("gulp"),
     watch = require("gulp-watch"),
     browserSync = require("browser-sync");
 
-var webroot = "./wwwroot/";
-var projectRoot = "./";
+var projectPaths = {
+    webroot: "./wwwroot/",
+    projectRoot: "./",
+    npmSrc: "./node_modules/",
+    npmLibs: "./wwwroot/lib/npmLibs/"
+};
 
-var paths = {
-    html: projectRoot + "Views/**/*.cshtml",
-    js: webroot + "js/**/*.js",
-    minJs: webroot + "js/**/*.min.js",
-    css: webroot + "css/**/*.css",
-    minCss: webroot + "css/**/*.min.css",
-    concatJsDest: webroot + "js/site.min.js",
-    concatCssDest: webroot + "css/site.min.css"
+var filePaths = {
+    js: projectPaths.webroot + "js/**/*.js",
+    minJs: projectPaths.webroot + "js/**/*.min.js",
+    css: projectPaths.webroot + "css/**/*.css",
+    minCss: projectPaths.webroot + "css/**/*.min.css",
+    concatJsDest: projectPaths.webroot + "js/site.min.js",
+    concatCssDest: projectPaths.webroot + "css/site.min.css",
+    html: projectPaths.projectRoot + "Views/**/*.cshtml"
 };
 
 // Cleaning
 gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
+    rimraf(filePaths.concatJsDest, cb);
 });
 
 gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
+    rimraf(filePaths.concatCssDest, cb);
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
 
 // Minifying
 gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
+    return gulp.src([filePaths.js, "!" + filePaths.minJs], { base: "." })
+        .pipe(concat(filePaths.concatJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
 
 gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
+    return gulp.src([filePaths.css, "!" + filePaths.minCss])
+        .pipe(concat(filePaths.concatCssDest))
         .pipe(cssmin())
         .pipe(gulp.dest("."));
 });
@@ -57,7 +61,36 @@ gulp.task("watch", function () {
         proxy: "http://localhost:56293/"
     });
 
-    gulp.watch(paths.js, ["min:js", browserSync.reload]);
-    gulp.watch(paths.css, ["min:css", browserSync.reload]);
-    gulp.watch(paths.html, [browserSync.reload]);
+    gulp.watch(filePaths.js, ["min:js", browserSync.reload]);
+    gulp.watch(filePaths.css, ["min:css", browserSync.reload]);
+    gulp.watch(filePaths.html, [browserSync.reload]);
 });
+
+// Tasks to copy NPM dependencies to the frontend library folder
+gulp.task("copy-deps:systemjs", function() {
+    return gulp.src(projectPaths.npmSrc + "/systemjs/dist/**/*.*", { base: projectPaths.npmSrc + "/systemjs/dist/" })
+         .pipe(gulp.dest(projectPaths.npmLibs + "/systemjs/"));
+});
+
+gulp.task("copy-deps:angular2", function () {
+    return gulp.src(projectPaths.npmSrc + "/angular2/bundles/**/*.js", { base: projectPaths.npmSrc + "/angular2/bundles/" })
+         .pipe(gulp.dest(projectPaths.npmLibs + "/angular2/"));
+});
+
+gulp.task("copy-deps:es6-shim", function () {
+    return gulp.src(projectPaths.npmSrc + '/es6-shim/es6-sh*', { base: projectPaths.npmSrc + '/es6-shim/' })
+         .pipe(gulp.dest(projectPaths.npmLibs + '/es6-shim/'));
+});
+
+gulp.task("copy-deps:es6-promise", function () {
+    return gulp.src(projectPaths.npmSrc + '/es6-promise/dist/**/*.*', { base: projectPaths.npmSrc + '/es6-promise/dist/' })
+         .pipe(gulp.dest(projectPaths.npmLibs + '/es6-promise/'));
+});
+
+gulp.task("copy-deps:rxjs", function () {
+    return gulp.src(projectPaths.npmSrc + '/rxjs/bundles/*.*', { base: projectPaths.npmSrc + '/rxjs/bundles/' })
+         .pipe(gulp.dest(projectPaths.npmLibs + '/rxjs/'));
+});
+
+gulp.task("copy-deps", [
+    "copy-deps:systemjs", "copy-deps:angular2", "copy-deps:es6-shim", "copy-deps:es6-promise", "copy-deps:rxjs"]);
