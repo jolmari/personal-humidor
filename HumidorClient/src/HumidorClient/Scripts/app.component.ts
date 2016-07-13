be6ea005dec84615f39110fa0cc4ebe7f404cf48
Kristian Virmo <kristian.virmo@digia.com>
2016-07-13 11:24:47 +0300

    One way / Two-way databinding testing.

--- HumidorClient/src/HumidorClient/Scripts/app.component.ts
+++ HumidorClient/src/HumidorClient/Scripts/app.component.ts
@@ -2,9 +2,27 @@
 
 @Component({
     selector: "my-app",
-    template: "<h2>Hello world!</h2>"
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{cigar.name}} details!</h2>
+        <div><label>id: </label>{{cigar.id}}</div>
+        <div><label>name: </label>{{cigar.name}}</div>
+        <div>
+            <label>Name: </label>
+            <input [(ngModel)]="cigar.name" placeholder="Name" />
+        </div>
+    `
 })
 
 export class AppComponent {
-    
+    title = "Cigar Catalogue";
+    cigar: Cigar = {
+        id: 1,
+        name: "Siglo V"
+    };
+}
+
+export class Cigar {
+    id: number;
+    name: string;
 }
