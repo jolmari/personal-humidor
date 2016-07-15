bd1f424690866f81341dbeac4f22c8c7d26859e9
Kristian Virmo <kristian.virmo@digia.com>
2016-07-15 14:28:38 +0300

    Separated cigars.component from app.component

--- HumidorClient/src/HumidorClient/scripts/components/app.component.ts
+++ HumidorClient/src/HumidorClient/scripts/components/app.component.ts
@@ -1,44 +1,17 @@
-﻿import { Component } from "@angular/core";
-import { OnInit } from "@angular/core";
-import { Cigar } from "../models/cigar";
-import { CigarDetailComponent } from "./cigar-detail.component";
+import { Component } from "@angular/core";
 import { CigarService } from "../services/cigar.service";
+import { CigarsComponent } from "./cigars.component";
 
 @Component({
     selector: "my-app",
     template: `
         <h1>{{title}}</h1>
-        <h2>Cigars</h2>
-        <ul class="cigars">
-            <li *ngFor="let cigar of cigars" (click)="onSelect(cigar)" [class.selected]="cigar === selectedCigar">
-                <span class="badge">{{cigar.id}}</span>{{cigar.name}}
-            </li>
-        </ul>
-        <cigar-detail [cigar]="selectedCigar"></cigar-detail>
+        <my-cigars></my-cigars>
     `,
-    directives: [CigarDetailComponent],
+    directives: [CigarsComponent],
     providers: [CigarService]
 })
 
-export class AppComponent implements OnInit {
-    constructor(private cigarService: CigarService) {}
-
-    ngOnInit(): any {
-        this.getCigars();
-    }
-
-    title = "The contents of your Humidor";
-
-    cigars: Cigar[];
-    selectedCigar: Cigar;
-
-    getCigars():void {
-        // assign the value when the given promise is resolved
-        this.cigarService.getCigars().then((cigars: Cigar[]) => this.cigars = cigars);
-    }
-
-    onSelect(cigar: Cigar):void {
-        this.selectedCigar = cigar;
-    }
+export class AppComponent {
+    title = "Boss of this Humidor";
 }
-
---
+++ HumidorClient/src/HumidorClient/scripts/components/cigars.component.ts
@@ -0,0 +1,43 @@
+﻿import { Component } from "@angular/core";
+import { OnInit } from "@angular/core";
+import { Cigar } from "../models/cigar";
+import { CigarDetailComponent } from "./cigar-detail.component";
+import {CigarService} from "../services/cigar.service";
+
+@Component({
+    selector: "my-cigars",
+    template: `
+        <h1>{{title}}</h1>
+        <h2>Cigars</h2>
+        <ul class="cigars">
+            <li *ngFor="let cigar of cigars" (click)="onSelect(cigar)" [class.selected]="cigar === selectedCigar">
+                <span class="badge">{{cigar.id}}</span>{{cigar.name}}
+            </li>
+        </ul>
+        <cigar-detail [cigar]="selectedCigar"></cigar-detail>
+    `,
+    directives: [CigarDetailComponent]
+})
+
+export class CigarsComponent implements OnInit {
+    constructor(private cigarService: CigarService) {}
+
+    ngOnInit(): any {
+        this.getCigars();
+    }
+
+    title = "The contents of your Humidor";
+
+    cigars: Cigar[];
+    selectedCigar: Cigar;
+
+    getCigars():void {
+        // assign the value when the given promise is resolved
+        this.cigarService.getCigars().then((cigars: Cigar[]) => this.cigars = cigars);
+    }
+
+    onSelect(cigar: Cigar):void {
+        this.selectedCigar = cigar;
+    }
+}
+
