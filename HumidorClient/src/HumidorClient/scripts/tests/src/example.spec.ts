43bc89ecde72be62904cad1873dc5270725cbf7f
Kristian Virmo <kristian.virmo@digia.com>
2016-07-20 11:20:44 +0300

    Barebones jasmine suite with typescript tests

---
+++ HumidorClient/src/HumidorClient/scripts/tests/src/example.spec.ts
@@ -0,0 +1,5 @@
+describe("placeholder tests",
+() => {
+    it("true is true", () => expect(true).toEqual(true));
+    it("null does not equal undefined", () => expect(null).toEqual(undefined));
+});
