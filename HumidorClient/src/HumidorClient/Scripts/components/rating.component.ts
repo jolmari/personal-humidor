27efed2b3240c7c25bccd7aef57e1a31227604fd
Kristian Virmo <kristian.virmo@digia.com>
2016-09-02 15:38:56 +0300

    Added rating to details in order to test 2 way databinding. Fixed off by one error that caused wrong ratings to be saved into API.

--- HumidorClient/src/HumidorClient/Scripts/components/rating.component.ts
+++ HumidorClient/src/HumidorClient/Scripts/components/rating.component.ts
@@ -1,10 +1,10 @@
-﻿import { Component, Input, OnInit } from "@angular/core";
+﻿import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
 
 @Component({
     selector: "rating",
     template: `
         <div class="rating-stars">
-            <template ngFor let-starState [ngForOf]="starStates" let-i="index">
+            <template ngFor [ngForOf]="starStates" let-i="index">
                 <i class="material-icons star" 
                    [ngClass]="{active: isStarActive(i) == true}"
                    (click)="updateRating(i)">star</i>
@@ -16,22 +16,22 @@
 export class RatingComponent implements OnInit {
     @Input() max: number = 5;
     @Input() rate: number = 0;
+    @Output() rateChange: EventEmitter<number> = new EventEmitter<number>();
+
     private starStates: any;
 
     ngOnInit(): void {
-        var result: any[] = [];
-        for (let i = 1; i <= this.max; ++i) {
-            result.push(i);
-        }
-
+        const result: Array<number> = new Array<number>(this.max);
         this.starStates = result;
     }
 
-    private isStarActive(index:number) {
-        return this.rate >= index;
+    isStarActive(index:number):boolean {
+        return this.rate > index;
     }
 
-    private updateRating(rating: number) {
+    updateRating(index: number):void {
+        let rating:number = index + 1;
         this.rate = rating;
+        this.rateChange.emit(rating);
     }
 }
