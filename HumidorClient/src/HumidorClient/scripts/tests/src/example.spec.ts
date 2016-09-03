3d973b2c4b8cfc0f7779f415ccffdbe0a58e2609
Jolmari <kristian.virmo@gmail.com>
2016-09-03 16:40:32 +0300

    Dropped tests for now, as the RC versions are updating sporadically

--- HumidorClient/src/HumidorClient/scripts/tests/src/example.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("placeholder tests",
-    function() {
-        it("true is true", function() { expect(true).toEqual(true) });
-        it("null does not equal undefined", function() { expect(null).not.toEqual(undefined) });
-});
