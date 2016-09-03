7e8cae4df49f9ec6fbee3adfe391fc1ba90ccb08
Jolmari <kristian.virmo@gmail.com>
2016-09-04 00:33:07 +0300

    Skeleton for the configuration provider

---
+++ HumidorClient/src/HumidorClient/Scripts/configs/config.ts
@@ -0,0 +1,25 @@
+﻿import { Observable } from "rxjs/Observable";
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+
+@Injectable()
+export class Config {
+
+    private configuration: any;
+    private environment: any;
+
+    constructor(private http: Http) {
+    }
+
+    load():void {
+        
+    }
+
+    getEnvironment(key: any): any {
+        
+    }
+
+    getConfiguration(key: any): any {
+        
+    }
+}
