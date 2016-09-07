1cdaffa4eabed5ad6ce51a6e4b5f98fa49400ada
Kristian Virmo <kristian.virmo@digia.com>
2016-09-07 17:44:53 +0300

    Added skeletons for the add cigar entry module

---
+++ HumidorClient/src/HumidorClient/Scripts/components/add-cigar-entry.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "add-cigar-entry",
+    template: `
+        <div class="col s12">
+            <p>Create should be here...</p>
+        </div>
+    `
+})
+
+export class AddCigarEntryComponent {
+    
+}
