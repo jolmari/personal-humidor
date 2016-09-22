8679b500523ca850e0ff0c1e282a9e63f3bc3c14
Kristian Virmo <kristian.virmo@digia.com>
2016-09-22 18:34:12 +0300

    Added ng2-select to enable drop down select list

--- HumidorClient/src/HumidorClient/Scripts/vendor.ts
+++ HumidorClient/src/HumidorClient/Scripts/vendor.ts
@@ -15,4 +15,6 @@ import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Angular2 bootstrap
-import "ng2-bootstrap/bundles/ng2-bootstrap";
+// TODO: ng2-bootstrap should be here, throws "System is not defined" error for some reason if
+// included in vendor.ts?
+import "ng2-select/bundles/ng2-select";
