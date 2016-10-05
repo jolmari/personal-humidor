58fd9eff047134ac973b532b2d7c8c082b9fe300
Kristian Virmo <kristian.virmo@digia.com>
2016-10-05 12:05:57 +0300

    Webpack config
    Split webpack into dev and test config files

---
+++ HumidorClient/src/HumidorClient/Webpack/webpack.test.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    devtool: "inline-source-map",
+    module: {
+        preLoaders: [
+            { exclude: /node_modules/, loader: "tslint", test: /\.ts$/ }
+        ],
+        loaders: [
+            { loader: "raw", test: /\.(css|scss|html)$/ },
+            { exclude: /node_modules/, loader: "ts", test: /\.ts$/ }
+        ],
+        resolve: {
+            extensions: ["", ".js", ".ts"],
+            modulesDirectories: ["node_modules"],
+            root: path.resolve(".", "Scripts")
+        },
+        tslint: {
+            emitErrors: true
+        }
+    }
+};
+
