f7c5b7f21502c8f187f6dda63e8e22e1d303f8c3
Jolmari <kristian.virmo@gmail.com>
2017-03-16 00:14:03 +0200

    angular-cli init

---
+++ humi-dashboard/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class HumiDashboardPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
