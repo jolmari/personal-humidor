e4afcdd948063adf3959070c67cf0bf95ebee4e7
Kristian Virmo <kristian.virmo@digia.com>
2017-04-03 15:47:17 +0300

    Removed frontend content from the .NET Core project

--- HumidorClient/src/HumidorClient/Scripts/main.ts
+++
@@ -1,6 +0,0 @@
-﻿import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import "ts-helpers";
-
-import {AppModule} from "./modules/app.module";
-
-platformBrowserDynamic().bootstrapModule(AppModule);
