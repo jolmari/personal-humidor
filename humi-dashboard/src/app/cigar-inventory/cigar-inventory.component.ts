948c129dc98862e66d24e967233588e8f4bccef4
Kristian Virmo <kristian.virmo@digia.com>
2017-04-03 10:32:09 +0300

    cigar-inventory files cleanup

--- humi-dashboard/src/app/cigar-inventory/cigar-inventory.component.ts
+++ humi-dashboard/src/app/cigar-inventory/cigar-inventory.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-//import { Cigar } from '../models/cigar';
-//import { CigarSearchService } from '../services/cigar-search.service';
-
 @Component({
     selector: 'humi-cigar-inventory',
     templateUrl: './cigar-inventory.component.html'
--- humi-dashboard/src/app/cigar-inventory/cigar-inventory.module.ts
+++ humi-dashboard/src/app/cigar-inventory/cigar-inventory.module.ts
@@ -5,37 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { CigarInventoryRouting } from './cigar-inventory-routing.module';
 import { CigarInventoryComponent } from './cigar-inventory.component';
 
-//import { RatingComponent } from "../components/rating.component";
-//import { CigarDetailsComponent } from "../components/cigar-details.component";
-//import { CigarSearchComponent } from "../components/cigar-search.component";
-//import { CigarsComponent } from "../components/cigars.component";
-
-//import { CigarService } from "../services/cigar.service";
-//import { CigarSearchService } from "../services/cigar-search.service";
-//import { EnvironmentService } from "../services/environment.service";
-
-//import { AddCigarModule } from "./add-cigar.module";
-//import { cigarInventoryRouting } from "../routes/cigar-inventory.routes";
-
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         CigarInventoryRouting
-        //AddCigarModule,
-        //cigarInventoryRouting
     ],
     declarations: [
-        CigarInventoryComponent,
-        //CigarDetailsComponent,
-        //CigarSearchComponent,
-        //RatingComponent,
-        //CigarsComponent
-    ],
-    providers: [
-        //CigarService,
-        //CigarSearchService,
-        //EnvironmentService
+        CigarInventoryComponent
     ]
 })
 
