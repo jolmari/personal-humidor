6af17ba1de20c95ec3199d6d66bbbcd55a2d29a4
Kristian Virmo <kristian.virmo@digia.com>
2017-04-07 16:39:36 +0300

    CigarService reformat, json -> cigar refactor

--- humi-dashboard/src/app/core/services/cigar.service.ts
+++ humi-dashboard/src/app/core/services/cigar.service.ts
@@ -1,51 +1,51 @@
-import { Injectable } from "@angular/core";
-import { Headers, Http, Response } from "@angular/http";
-import { Observable } from "rxjs/Rx";
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
-import { EnvironmentService } from "./environment.service";
 import { Cigar } from 'app/shared/models/cigar';
+import { EnvironmentService } from './environment.service';
 
 @Injectable()
 export class CigarService {
-    private cigarBaseUrl = "/api/cigars";
+    private cigarBaseUrl = '/api/cigars';
     private headers = new Headers({
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
     });
 
     constructor(private http: Http, private environmentService: EnvironmentService) { }
 
-    getAll(): Observable<Cigar[]> {
+    public getAll(): Observable<Cigar[]> {
         return this.getFromUrl(this.cigarBaseUrl);
     }
 
-    get(id: number): Observable<Cigar> {
-        const url: string = `${this.cigarBaseUrl}/${id}`;
+    public get(id: number): Observable<Cigar> {
+        const url = `${this.cigarBaseUrl}/${id}`;
         return this.getFromUrl(url);
     }
 
-    delete(cigar: Cigar): Observable<any> {
-        const url: string = `${this.cigarBaseUrl}/${cigar.id}`;
+    public delete(cigar: Cigar): Observable<any> {
+        const url = `${this.cigarBaseUrl}/${cigar.id}`;
         return this.deleteFromUrl(url);
     }
 
-    create(cigar: Cigar): Observable<any> {
+    public create(cigar: Cigar): Observable<any> {
         cigar.id = 0;
         return this.postToUrl(this.cigarBaseUrl, cigar);
     }
 
-    edit(cigar: Cigar): Observable<any> {
-        const url: string = `${this.cigarBaseUrl}/${cigar.id}`;
+    public edit(cigar: Cigar): Observable<any> {
+        const url = `${this.cigarBaseUrl}/${cigar.id}`;
         return this.putToUrl(url, cigar);
     }
 
-    private postToUrl(url:string, cigar:Cigar): Observable<Cigar> {
+    private postToUrl(url: string, cigar: Cigar): Observable<Cigar> {
         return this.http
             .post(url, JSON.stringify(cigar), { headers: this.headers })
             .map(this.extractData)
             .catch(this.handleError);
     }
 
-    private putToUrl(url:string, cigar: Cigar): Observable<Response> {
+    private putToUrl(url: string, cigar: Cigar): Observable<Response> {
         return this.http
             .put(url, JSON.stringify(cigar), { headers: this.headers })
             .catch(this.handleError);
@@ -64,13 +64,15 @@ export class CigarService {
             .catch(this.handleError);
     }
 
-    private extractData(res: Response):any {
-        return res.json() || { };
+    private extractData(res: Response): any {
+        return res
+            .json()
+            .map(raw => new Cigar().fromJson(raw));
     }
 
     private handleError(error: any): Observable<any> {
-        const errorMsg:string = (error.message || error.status) ?
-            `${error.status} - ${error.message}` : "Unknown server error";
+        const errorMsg: string = (error.message || error.status) ?
+            `${error.status} - ${error.message}` : 'Unknown server error';
 
         return Observable.throw(errorMsg);
     }
